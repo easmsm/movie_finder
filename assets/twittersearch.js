9a81ed1912a7b7ef2f407f10cf9922e8da6cf85e
ekukwa <easmsm@gmail.com>
2022-07-08 16:59:30 -0400

    added search button with onclick temporarily to test

--- assets/twittersearch.js
+++ assets/twittersearch.js
@@ -2,35 +2,14 @@
 let movieSearch = document.getElementById("movie-search")
 let twitterSearch = document.getElementById("twitter-search")
 let searchHashTag = document.getElementById("search-hash-tag")
-let search = document.getElementById("search")
+//let search = document.getElementById("search")
 
+function search () {
+    twitterSearch.textContent = 'https://api.twitter.com/2/tweets/search/recent?query=%23movies'
+}
 
-//source https://github.com/tombaranowicz/TwitterMonitoringJavaScript/blob/master/index.js, using Postman to generate urls
+// const apikey = 'uvVg9Ad90bgLgMeEZCrzhSaFg'
+// const apiSecretKey = 'yHMOcBgS1ah7zRC38oXVUE1ydwKRif6MJmcqm72jSB6Ku1U9fr'
+// const accessToken = '900833742218043392-40uWRnJBGPnJOWVF16pddcOERLMHJ6t'
+// const accessTokenSecret = '8K8apSfZZtOd89XmhPBLDW4NMPfEsUxhZDuamUCN3QsJ5'
 
-const Twit = require('twit')
-const notifier = require('node-notifier');
-const open = require('open');
-const franc = require('franc')
-
-const apikey = 'uvVg9Ad90bgLgMeEZCrzhSaFg'
-const apiSecretKey = 'yHMOcBgS1ah7zRC38oXVUE1ydwKRif6MJmcqm72jSB6Ku1U9fr'
-const accessToken = '900833742218043392-40uWRnJBGPnJOWVF16pddcOERLMHJ6t'
-const accessTokenSecret = '8K8apSfZZtOd89XmhPBLDW4NMPfEsUxhZDuamUCN3QsJ5'
-
-var T = new Twit({
-  consumer_key:         apikey,
-  consumer_secret:      apiSecretKey,
-  access_token:         accessToken,
-  access_token_secret:  accessTokenSecret,
-});
-
-(async () => {
-   // //1. GET RECENT TWEETS
-T.get('search/tweets', { q: '#movies since:2022-07-01s', count: 100 }, function(err, data, response) {
-const tweets = data.statuses
-.map(tweet => `LANG: ${franc(tweet.text)} : ${tweet.text}`) //CHECK LANGUAGE
-.map(tweet => tweet.text)
-.filter(tweet => tweet.toLowerCase().includes('elon'));
-console.log(tweets);
-    })
-})
