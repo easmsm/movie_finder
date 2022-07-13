f5330424bf8c75bd9c09150ef23bf7eb64ddb320
Forest Carter <fc_vstate@yahoo.com>
2022-07-13 17:21:02 -0400

    updated media shelf

--- assets/homepage.js
+++ assets/homepage.js
@@ -1,7 +1,7 @@
 // variables for DOM elements 
 var inputEl = document.querySelector(".input")
 var generateBtnEl = document.querySelector(".generate")
-var saveBtnEl = document.querySelector(".save")
+var savebtnMovieEl = document.querySelector(".save")
 var savedMoviesUlEl = document.querySelector(".savedMovies")
 var googlebtnEl = document.querySelector(".google")
 // var savedBookUlEl = document.querySelector(".savedBooks")
@@ -9,8 +9,46 @@ var generateBookBtnEl = document.querySelector(".generateBook")
 var inputBookEl = document.querySelector(".inputBook")
 var googleBookBtnEl = document.querySelector(".googleBook")
 var savebtnBookEl = document.querySelector(".saveBook")
-var saveBooksUlEl = document.querySelector(".savedBooks")
+var savedBooksUlEl = document.querySelector(".savedBooks")
+//food search variable
+var foodSearchBtnEl = document.querySelector(".grubhubBtn");
 
+
+
+//this will happen on page load
+// get data from local storage and put it on page
+function onPageLoad() {
+    // first, we need to get the 'moviesList' data from localstorage. Remember, this data is an array
+    var savedMovies = JSON.parse(localStorage.getItem("moviesList") || "[]");
+     console.log(savedMovies);
+
+    /// because we have an array of data, we need to loop over each item in the array
+        for (var i = 0; i < savedMovies.length; i++) {
+            // in a for loop, the current thiing you are loopiing over is savedMovies[i]
+            console.log(savedMovies[i]);
+
+    //loads movies in "saved movies list"
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(savedMovies[i]));  //savedMovies[i] is the name of the movie to put on the page
+        savedMoviesUlEl.appendChild(li);
+
+        }
+    
+    var savedBooks = JSON.parse(localStorage.getItem("booksList") || "[]");
+            console.log(savedBooks);
+
+        for (var i = 0; i < savedBooks.length; i++) {
+            console.log(savedBooks);
+
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(savedBooks[i]));
+        savedBooksUlEl.appendChild(li);
+
+        }
+
+}
+
+onPageLoad()
 
 
 // Movies 
@@ -58,10 +96,36 @@ function randomIndex(array) {
 
 
 // save button click function 
-saveBtnEl.addEventListener("click", function () {
+savebtnMovieEl.addEventListener("click", function () {
+
+    //save movie in local storage -->
+
+    // first, need to get data in local storage
+    var savedMoviesList = JSON.parse(localStorage.getItem("moviesList") || "[]");  // || means "or". If there is nothing in local storage, return empty ARRAY
+    console.log('Saved Movies List:', savedMoviesList);
+
+    // we only want 10 movies on the moives list. IF there are more than 10, remove the oldest one and add a new on
+    // check how many movies are in the movies list
+    if (savedMoviesList.length > 7) {
+        // we neeed to remove the oldest movie because we can only have 10 movies
+        savedMoviesList = savedMoviesList.slice(1)  // slice this removes first item(movie) in the array
+        savedMoviesList.push(inputEl.innerHTML)  // push adds new item(movie) to end of array
+    } else {
+       //  need to put data into savedMoviesList
+        // remember, savedMoviesList is an array!
+        savedMoviesList.push(inputEl.innerHTML)
+    }
+
+    
+    //last, set savedMoviesList into local storage
+    localStorage.setItem("moviesList", JSON.stringify(savedMoviesList));
+
+
+    //saves the movie in 'saved movies' list
     var li = document.createElement("li");
     li.appendChild(document.createTextNode(inputEl.innerHTML));
     savedMoviesUlEl.appendChild(li);
+
 })
 
 
@@ -114,14 +178,44 @@ generateBookBtnEl.addEventListener("click", function () {
 
 })
 
+savebtnBookEl.addEventListener("click", function() {
+
+    //save book in local storage -->
+
+    //get data in local storage
+    var savedBooksList = JSON.parse(localStorage.getItem("booksList") || "[]");
+        console.log("Saved Books List", savedBooksList);
+    
+    // we only want 10 Books on the Book list. IF there are more than 10, remove the oldest one and add a new on
+    //check how many books are in the list
+    if  (savedBooksList.length > 1) {
+        savedBooksList = savedBooksList.slice(1) // slice removes first item(book) in the array
+        savedBooksList.push(inputBookEl.innerHTML) // push adds new item(book) to end of array
+    
+    }else{
+        // next, need to put data into savedBooksList
+        // remember, savedBooksList is an array!
+       savedBooksList.push(inputBookEl.innerHTML)
+
+     }
+
+    //last, set savedBooksList into local storage
+    localStorage.setItem("booksList", JSON.stringify(savedBooksList));
+
+     //saves the book in 'saved books' list
+     var li = document.createElement("li");
+     li.appendChild(document.createTextNode(inputBookEl.innerHTML));
+     savedBooksUlEl.appendChild(li);
+})
+
 
 // save button click function for book
-savebtnBookEl.addEventListener("click", function () {
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(inputBookEl.innerHTML));
-    saveBooksUlEl.appendChild(li);
+// savebtnBookEl.addEventListener("click", function () {
+//     var li = document.createElement("li");
+//     li.appendChild(document.createTextNode(inputBookEl.innerHTML));
+//     savedBooksUlEl.appendChild(li);
 
-})
+// })
 
 
 //google button event listener to open new tabs and search generated book
@@ -129,6 +223,12 @@ googleBookBtnEl.addEventListener("click", function () {
     window.open('http://www.google.com/search?q=' + inputBookEl.innerHTML);
 
 })
+
+foodSearchBtnEl.addEventListener("click", function() {
+
+    window.open("https://grubhub.com"); 
+
+ })
 
 
 
