0bf1795f04d8c891711e923fc20247185dbaab32
Mike <mike.levy.rva@gmail.com>
2022-07-16 16:15:54 -0400

    delete list item functionality

--- assets/homepage.js
+++ assets/homepage.js
@@ -20,6 +20,9 @@ var deleteMovieListBtnEl = document.querySelector(".deleteSavedMoviesBtn")
 const burgerIcon = document.querySelector('#burger');
 const navbarMenu = document.querySelector('#nav-links');
 
+var savedBooksList = JSON.parse(localStorage.getItem("booksList") || "[]");
+var savedMoviesList = JSON.parse(localStorage.getItem("moviesList") || "[]");
+
 burgerIcon.addEventListener('click', () => {
     navbarMenu.classList.toggle('is-active');
 });
@@ -51,12 +54,14 @@ function onPageLoad() {
     /// because we have an array of data, we need to loop over each item in the array
         for (var i = 0; i < savedMovies.length; i++) {
             // in a for loop, the current thing you are looping over is savedMovies[i]
-            console.log(savedMovies[i]);
+            console.log(savedMovies);
 
-    //loads movies in "saved movies list"
-        var li = document.createElement("li");
-        li.appendChild(document.createTextNode(savedMovies[i]));  //savedMovies[i] is the name of the movie to put on the page
-        savedMoviesUlEl.appendChild(li);
+            deleteBtnMovie(savedMovies[i])
+
+    // //loads movies in "saved movies list"
+    //     var li = document.createElement("li");
+    //     li.appendChild(document.createTextNode(savedMovies[i]));  //savedMovies[i] is the name of the movie to put on the page
+    //     savedMoviesUlEl.appendChild(li);
 
         }
     
@@ -66,16 +71,16 @@ function onPageLoad() {
         for (var i = 0; i < savedBooks.length; i++) {
             console.log(savedBooks);
 
-        var li = document.createElement("li");
-        li.appendChild(document.createTextNode(savedBooks[i]));
-        savedBooksUlEl.appendChild(li);
+        // var li = document.createElement("li");
+        // li.appendChild(document.createTextNode(savedBooks[i]));
+        // savedBooksUlEl.appendChild(li);
+        deleteBtn(savedBooks[i])
 
         }
 
 }
 
 onPageLoad()
-
 
 // Movies 
 // Movies 
@@ -97,7 +102,7 @@ var fetchFunction = function () {
         .then(response => {
             // console.log(response.results[0])
             var movie = randomIndex(response.results)
-            inputEl.innerHTML = response.results[movie].title
+            inputEl.textContent = response.results[movie].title
             console.log(response.results[movie].title)
 
         })
@@ -109,17 +114,17 @@ var fetchFunction = function () {
 // }
 
 
+var movieKey = 0
+
 // Search button function to call fetch function to get a generated movie every click
 generateBtnEl.addEventListener("click", function () {
     fetchFunction();
     inputEl.classList.remove("opacity")
 })
 
-
 function randomIndex(array) {
     return Math.floor(Math.random() * array.length)
 }
-
 
 // save button click function 
 savebtnMovieEl.addEventListener("click", function () {
@@ -127,7 +132,7 @@ savebtnMovieEl.addEventListener("click", function () {
     //save movie in local storage -->
 
     // first, need to get data in local storage
-    var savedMoviesList = JSON.parse(localStorage.getItem("moviesList") || "[]");  // || means "or". If there is nothing in local storage, return empty ARRAY
+    // var savedMoviesList = JSON.parse(localStorage.getItem("moviesList") || "[]");  // || means "or". If there is nothing in local storage, return empty ARRAY
         console.log("Saved Movies List", savedMoviesList);
     // we only want 10 movies on the movies list. IF there are more than 10, no more should be added to the list
     // check how many movies are in the movies list
@@ -138,16 +143,53 @@ savebtnMovieEl.addEventListener("click", function () {
     } else {
     //  need to put data into savedMoviesList
      // remember, savedMoviesList is an array!
-        savedMoviesList.push(inputEl.innerHTML)
+        savedMoviesList.push(inputEl.textContent)
+        deleteBtnMovie(inputEl.textContent)
         //saves the movie in 'saved movies' li
-        var li = document.createElement("li");
-        li.appendChild(document.createTextNode(inputEl.innerHTML));
-        savedMoviesUlEl.appendChild(li);
+        // var li = document.createElement("li");
+        // li.appendChild(document.createTextNode(inputEl.innerHTML));
+        // savedMoviesUlEl.appendChild(li);
 
         //last, set savedMoviesList into local storage
         localStorage.setItem("moviesList", JSON.stringify(savedMoviesList));
     }
 })
+
+
+
+
+function deleteBtnMovie(title){
+    //saves the book in 'saved books' list
+   var li = document.createElement("li");
+   var singleDeleteBtn = document.createElement("button")
+   singleDeleteBtn.className = "singleItemDelete"
+   singleDeleteBtn.textContent = "Delete"
+
+   li.appendChild(document.createTextNode(title));
+   singleDeleteBtn.addEventListener("click", function() {
+       savedMoviesUlEl.removeChild(li); 
+   var filteredMovies =  savedMoviesList.filter((name)=> name !== title)
+    localStorage.setItem("moviesList", JSON.stringify(filteredMovies));
+   })
+   li.appendChild(singleDeleteBtn)
+   savedMoviesUlEl.appendChild(li);
+   movieKey++
+  
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //google click function
@@ -159,11 +201,9 @@ googlebtnEl.addEventListener("click", function () {
 
 
 
-
 // BOOK SECTION
 // BOOK SECTION
 // BOOK SECTION
-
 
 //rapidAPI key / host
 const options = {
@@ -174,60 +214,92 @@ const options = {
     }
 };
 
-
 //fetch function for generating random book
 var bookFetchFunction = function () {
     fetch('https://bookshelves.p.rapidapi.com/books', options)
         .then(response => response.json())
         .then(response => {
             // console.log(response.results[0])
-            inputBookEl.innerHTML = response.Books[Math.floor(Math.random() * 37)].title
-
+            inputBookEl.textContent = response.Books[Math.floor(Math.random() * 37)].title
+            
             console.log(response.Books[Math.floor(Math.random() * 37)].title)
-
+            
         })
         .catch(err => console.error(err));
-}
-
-
+    }
+    
+    
+    var bookKey = 0
 
 // Search button function to call fetch function to get a generated book every click
 generateBookBtnEl.addEventListener("click", function () {
     bookFetchFunction();
     inputBookEl.classList.remove("opacity")
 
-
 })
 
 savebtnBookEl.addEventListener("click", function() {
-    console.log();
+    console.log(inputBookEl.textContent);
 
     //save book in local storage -->
 
     //get data in local storage
-    var savedBooksList = JSON.parse(localStorage.getItem("booksList") || "[]");
+    // var savedBooksList = JSON.parse(localStorage.getItem("booksList") || "[]");
         console.log("Saved Books List", savedBooksList);
     
-    // we only want 10 Books on the Book list. IF there are more than 10, no more should be added
-    //check how many books are in the list
+    // // we only want 10 Books on the Book list. IF there are more than 10, no more should be added
+    // //check how many books are in the list
     if  (savedBooksList.length >= 10) {
         savedBooksList = savedBooksList.slice(0,0) // slice: this prevents item(book) to be added to array after 10 items
      }else{
         // next, need to put data into savedBooksList
         // remember, savedBooksList is an array!
-        savedBooksList.push(inputBookEl.innerHTML)
 
-         //saves the book in 'saved books' list
-        var li = document.createElement("li");
-        li.appendChild(document.createTextNode(inputBookEl.innerHTML));
-        savedBooksUlEl.appendChild(li);
+        savedBooksList.push(inputBookEl.textContent)
+deleteBtn(inputBookEl.textContent)
+    //      //saves the book in 'saved books' list
+    //     var li = document.createElement("li");
+    //     var singleDeleteBtn = document.createElement("button")
+    //     singleDeleteBtn.className = "singleItemDelete"
+    //     singleDeleteBtn.textContent = "Delete"
+    //     li.appendChild(document.createTextNode(inputBookEl.textContent));
+    //     li.appendChild(singleDeleteBtn)
+    //     savedBooksUlEl.appendChild(li);
+
+    //     singleDeleteBtn.addEventListener("click", function() {
+    //         savedBooksUlEl.removeChild(li);
+    //         localStorage.setItem("booksList", JSON.stringify(savedBooksList));
+            
     
-        //last, set savedBooksList into local storage
+    //      })
+    
+    //     //last, set savedBooksList into local storage
         localStorage.setItem("booksList", JSON.stringify(savedBooksList));
 
      }
 
 })
+
+function deleteBtn(title){
+         //saves the book in 'saved books' list
+        var li = document.createElement("li");
+        var singleDeleteBtn = document.createElement("button")
+        singleDeleteBtn.className = "singleItemDelete"
+        singleDeleteBtn.textContent = "Delete"
+  
+        li.appendChild(document.createTextNode(title));
+        singleDeleteBtn.addEventListener("click", function() {
+            savedBooksUlEl.removeChild(li); 
+        var filteredBooks =  savedBooksList.filter((name)=> name !== title)
+         localStorage.setItem("booksList", JSON.stringify(filteredBooks));
+        })
+        li.appendChild(singleDeleteBtn)
+        savedBooksUlEl.appendChild(li);
+        bookKey++
+       
+
+}
+
 
 
 //google button event listener to open new tabs and search generated book
@@ -241,6 +313,7 @@ foodSearchBtnEl.addEventListener("click", function() {
      window.open("https://grubhub.com"); 
 
  })
+
 
 
 
